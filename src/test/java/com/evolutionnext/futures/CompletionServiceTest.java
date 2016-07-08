23883bd41ef4e16906d8f01dc5a15f3b5db1c558
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-07-08 11:02:48 -0500

    updated Slide Deck

---
+++ src/test/java/com/evolutionnext/futures/CompletionServiceTest.java
@@ -0,0 +1,11 @@
+package com.evolutionnext.futures;
+
+/**
+ * @author Daniel Hinojosa
+ * @since 7/8/16 9:16 AM
+ * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
+ * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
+ * tel: 505.363.5832
+ */
+public class CompletionServiceTest {
+}
